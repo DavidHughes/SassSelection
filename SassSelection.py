7de11086ad1581ae24d9e90542136e98fad73fce
David Hughes <david.hughes@cancer.org.uk>
2013-10-25 16:08:43 +0100

    report_expiring_message -> report_expiring_status

--- SassSelection.py
+++ SassSelection.py
@@ -9,7 +9,7 @@ class SassSelectionCommand(sublime_plugin.TextCommand):
     # Traverse selector tree to root (i.e. no indent)
     # 'Compile' selector to true CSS
 
-    self.report_expiring_message('message', 'Found: %s' % (self.view.substr(selection)))
+    self.report_expiring_status('message', 'Found: %s' % (self.view.substr(selection)))
 
   # Returns first selection region if it covers only one row
   def validate_selection(self):
@@ -25,10 +25,10 @@ class SassSelectionCommand(sublime_plugin.TextCommand):
     if begin_row == end_row:
       return first_region
     else:
-      self.report_expiring_message('error', 'Multi-row regions are not supported', 5000)
+      self.report_expiring_status('error', 'Multi-row regions are not supported', 5000)
       raise Exception('Multi-row region was selected, logic for this not yet determined')
 
 
-  def report_expiring_message(self, status_key, status_message, timeout=3000):
+  def report_expiring_status(self, status_key, status_message, timeout=3000):
     self.view.set_status(status_key, status_message)
     sublime.set_timeout(lambda: self.view.erase_status(status_key), timeout)
